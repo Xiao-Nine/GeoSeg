ef987cd23797d1444a4ca87fc4d73171dcb99808
Xiao-Nine <528361423@qq.com>
2024-10-04 06:23:41 +0000

    one

--- config/loveda/unetformer.py
+++ config/loveda/unetformer.py
@@ -27,7 +27,7 @@ save_top_k = 1
 save_last = True
 check_val_every_n_epoch = 1
 pretrained_ckpt_path = None # the path for the pretrained model weight
-gpus = 'auto'  # default or gpu ids:[0] or gpu nums: 2, more setting can refer to pytorch_lightning
+gpus = [2]  # default or gpu ids:[0] or gpu nums: 2, more setting can refer to pytorch_lightning
 resume_ckpt_path = None  # whether continue training with the checkpoint, default None
 
 #  define the network
